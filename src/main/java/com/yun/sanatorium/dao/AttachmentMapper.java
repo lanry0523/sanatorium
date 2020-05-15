a0fd6529c9d3380328f1f1bc5e38ced25a02b6b3
lanry0523 <sendou00002@163.com>
2020-05-15 18:34:40 +0800

    文件类dao

--- src/main/java/com/yun/sanatorium/dao/AttachmentMapper.java
+++ src/main/java/com/yun/sanatorium/dao/AttachmentMapper.java
@@ -3,5 +3,12 @@ package com.yun.sanatorium.dao;
 import com.yun.sanatorium.core.Mapper;
 import com.yun.sanatorium.model.entity.Attachment;
 
+import java.util.List;
+
 public interface AttachmentMapper extends Mapper<Attachment> {
+    /**
+     * 批量保存附件
+     * @param list
+     */
+    void batchSaveAttachmentList(List<Attachment> list);
 }
